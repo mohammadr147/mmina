7d5588e2e301c8c8ca00959abc95c21d38a4dc10
Mohammadr147 <Mohammadr147@gmail.com>
2020-11-21 14:59:35 +0330

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+           public IActionResult Index(string uname, string pwd)
+        {
+            if(uname == "mina" && pwd == "Mina")
+            {
+               ViewData["token"] = "m";
+                return View("welcome");
+
+            }
+
+            return View();
+        }
+
+        public IActionResult welcome()
+        {
+            if(ViewData["token"]=="m")
+            {
+            return View();
+            }
+            else{
+                return View("Index");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
